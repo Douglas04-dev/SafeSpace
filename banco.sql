3e03911a360cf05c11f1be9487e5dd13332ae8c6
Rafael Brandão <rafaelbranda295@gmail.com>
2025-05-14 21:21:45 -0300

    adicionando a tabela das fotos

--- banco.sql
+++ banco.sql
@@ -21,8 +21,8 @@ DROP TABLE IF EXISTS `usuarios`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
   `idUsuarios` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `senha` varchar(50) NOT NULL,
   PRIMARY KEY (`idUsuarios`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -52,11 +52,15 @@ DROP TABLE IF EXISTS `profissionais`;
 CREATE TABLE `profissionais` (
   `idProfissional` int(11) NOT NULL AUTO_INCREMENT,
   `idUsuario` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL,
   `cpf` char(11) NOT NULL,
-  `crp` varchar(20) NOT NULL,
-  `especialidade` varchar(100) DEFAULT NULL,
+  `data_nascimento` date NOT NULL,
+  `crp` char(8) NOT NULL,
+  `especialidade` varchar(300) DEFAULT NULL,
   `telefone` varchar(20) DEFAULT NULL,
   `descricao` text DEFAULT NULL,
+  `link_agenda` varchar(300) NOT NULL,
   PRIMARY KEY (`idProfissional`),
   UNIQUE KEY `cpf_UNIQUE` (`cpf`),
   UNIQUE KEY `crp_UNIQUE` (`crp`),
@@ -64,6 +68,19 @@ CREATE TABLE `profissionais` (
   CONSTRAINT `fk_usuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuarios`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+CREATE TABLE `fotos_perfil` (
+  `idFoto` INT NOT NULL AUTO_INCREMENT,
+  `idDono` INT NOT NULL,
+  `tipo_dono` ENUM('usuario', 'profissional') NOT NULL,
+  `caminho_arquivo` VARCHAR(255) NOT NULL,
+  `data_upload` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idFoto`)
+);
+
+
 
 --
 -- Dump completed on [DATA ATUAL]
