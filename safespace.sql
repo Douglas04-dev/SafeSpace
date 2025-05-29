99363025ceb514d8804d6b9bf5c6e059dcf6043e
Douglas04-dev <douglas14062004@gmail.com>
2025-05-28 21:17:32 -0300

    Update safespace.sql

--- safespace.sql
+++ safespace.sql
@@ -1,20 +1,5 @@
-CREATE DATABASE IF NOT EXISTS `safespace` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+CREATE DATABASE IF NOT EXISTS `safespace`;
 USE `safespace`;
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `usuarios`
---
 
 DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -27,8 +12,8 @@ CREATE TABLE `usuarios` (
   PRIMARY KEY (`idUsuarios`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+ALTER TABLE profissionais ADD COLUMN nome VARCHAR(50) NOT NULL AFTER idUsuario;
 
---
 -- Dumping data for table `usuarios`
 --
 
@@ -46,13 +31,12 @@ UNLOCK TABLES;
 -- Table structure for table `profissionais`
 --
 
-DROP TABLE IF EXISTS `profissionais`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `profissionais` (
   `idProfissional` int(11) NOT NULL AUTO_INCREMENT,
-  `idUsuario` int(11) NOT NULL,
+
   `cpf` char(11) NOT NULL,
+    `nome` char(11) NOT NULL,
   `crp` varchar(20) NOT NULL,
   `especialidade` varchar(100) DEFAULT NULL,
   `telefone` varchar(20) DEFAULT NULL,
@@ -74,27 +58,3 @@ CREATE TABLE `fotos_perfil` (
   `data_upload` DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`idFoto`)
 );
-
-CREATE TABLE bio_profissional (
-  'idBio' INT NOT NULL AUTO_INCREMENT,
-  'idProfissional' INT NOT NULL,
-  'texto' TEXT DEFAULT NULL,         
-  PRIMARY KEY ('idBio'),
-  UNIQUE KEY 'unique_profissional' ('idProfissional'),
-  CONSTRAINT 'fk_bio_profissional' FOREIGN KEY ('idProfissional') REFERENCES 'profissionais'('idProfissional') ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
-
---
--- Dump completed on [DATA ATUAL]
-*/
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
